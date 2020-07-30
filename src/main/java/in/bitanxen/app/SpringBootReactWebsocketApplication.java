93335b2f9b3f1458eb5619f87d7aec732097b58a
Bitan Biswas <bitanxen@nyx-micasa.local>
2020-07-30 23:22:18 +0800

    project created

---
+++ src/main/java/in/bitanxen/app/SpringBootReactWebsocketApplication.java
@@ -0,0 +1,13 @@
+package in.bitanxen.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactWebsocketApplication.class, args);
+	}
+
+}
