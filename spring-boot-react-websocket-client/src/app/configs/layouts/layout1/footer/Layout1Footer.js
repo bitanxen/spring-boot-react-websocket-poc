eddd7730db03c1ce61fe72a8052023cf63d718d7
Bitan Biswas <bitanxen@gmail.com>
2020-08-14 13:59:13 +0800

    chat working

--- spring-boot-react-websocket-client/src/app/configs/layouts/layout1/footer/Layout1Footer.js
+++ spring-boot-react-websocket-client/src/app/configs/layouts/layout1/footer/Layout1Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Layout1Footer(props) {
   return (
-    <div className="absolute w-full text-center z-9999 shadow-1 p-3 text-black bottom-0 bg-blue-100">
+    <div className="w-full text-center z-9999 shadow-1 p-3 text-black bottom-0 bg-blue-100">
       Developed and Managed By Bitan Biswas
     </div>
   );
